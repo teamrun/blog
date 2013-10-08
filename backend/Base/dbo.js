f75e31a99af6e72ef62e0a3ab7f9a4353c19a94c
陈 亮亮 <chenllos@163.com>
2013-10-08 19:04:17 +0800

    进一步完善exec执行mongod,先检查mongo命令的stderr.再执行初始化数据库的操作

--- backend/Base/dbo.js
+++ backend/Base/dbo.js
@@ -4,58 +4,69 @@ var config = require('./config');
 var child = require('child_process')
     , exec = child.exec;
 
- exec('mongod', function( err, stdout, stderr){
- 	if( !err && !stderr ){
- 		var connect = mongoose.connect('mongodb://localhost/blog');
+exec('mongo', function(err, stdout, stderr){
+	if( stderr ){
+		startMongod();
+	}
+	else{
+		initDB();
+	}
+});
 
-		var blogSchema = mongoose.Schema({
-			title: String,
-			alia: String,
-			content: String,
-			summery: Object,
-			dt_create: Date,
-			dt_modify: Array,
-			author: String,
-			location: Object,
-			like: Number,
-			comment: Number,
-			tag: Array
-		});
+function startMongod(){
+	exec('mongod', function(err, stdout, stderr){
+		if( !err && !stderr ){
+			initDB();
+		}
+	} );
+}
+function initDB(){
+	
+	var connect = mongoose.connect('mongodb://localhost/blog');
+	var blogSchema = mongoose.Schema({
+		title: String,
+		alia: String,
+		content: String,
+		summery: Object,
+		dt_create: Date,
+		dt_modify: Array,
+		author: String,
+		location: Object,
+		like: Number,
+		comment: Number,
+		tag: Array
+	});
 
-		var Blog = mongoose.model('blog', blogSchema);
+	var Blog = mongoose.model('blog', blogSchema);
 
-		var firstBlog = new Blog({
-			title: '搭建自己的博客',
-			content: '还在想',
-			dt_create: new Date(),
-			dt_modify: [ new Date() ],
-			author: 'chenllos',
-			location: null,
-			like: 0,
-			comment: 0,
-			tag: ['tech']
-		});
-		// firstBlog.save();
+	var firstBlog = new Blog({
+		title: '搭建自己的博客',
+		content: '还在想',
+		dt_create: new Date(),
+		dt_modify: [ new Date() ],
+		author: 'chenllos',
+		location: null,
+		like: 0,
+		comment: 0,
+		tag: ['tech']
+	});
+	// firstBlog.save();
 
-		// 标题，内容，评论人，评论谁， 基于哪篇文章
-		var commentSchema = mongoose.Schema({
-			title: String,
-			content: String,
-			dt_create: String,
-			from: String,
-			to: String,
-			base_article: String,
-			base_cmt: String
-			// subcom
-		});
+	// 标题，内容，评论人，评论谁， 基于哪篇文章
+	var commentSchema = mongoose.Schema({
+		title: String,
+		content: String,
+		dt_create: String,
+		from: String,
+		to: String,
+		base_article: String,
+		base_cmt: String
+		// subcom
+	});
 
-		var Comment = mongoose.model( 'comment', commentSchema );
+	var Comment = mongoose.model( 'comment', commentSchema );
+	
 
-
-
-		exports.Blog = Blog;
-		exports.Comment = Comment;
- 	}
- });
-
-
+	exports.Blog = Blog;
+	exports.Comment = Comment;
+}
