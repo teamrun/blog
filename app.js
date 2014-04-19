c62f65c02652f40c8e7cce8ae4c6546b251a261c
teamrun <chenllos@163.com>
2014-04-19 23:31:54 +0800

    添加: 压缩 和 缓存

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+var path = require('path');
+
 var express = require('express');
 var bodyParser = require('body-parser');
-var path = require('path');
+var cookieParser = require('cookie-parser');
+var compress = require( 'compression' );
 
 var config = require('./backend/config');
 var route = require('./backend/route');
@@ -11,15 +14,25 @@ var app = express();
 
 // 使用ejs解析引擎
 app.set('view engine', 'jade');
-// app.set('view engine', 'jade');
-app.set('views', path.join(__dirname, 'views'));
+
 // 视图文件都在/views
+app.set('views', path.join(__dirname, 'views'));
 
-
+// 参数解析, 文件上传
 app.use( bodyParser() );
-// app.use(express.cookieParser());
+app.use( cookieParser() );
+
+// 使用压缩
+// Respense Header:
+//      Content-Encoding:gzip
+// compress() should have been included **Before** the static file server
+app.use( compress() );
+
 // 默认的js css等静态资源根目录
-app.use(express.static( __dirname + '/public'));
+// 添加缓存控制
+
+var cacheTime = 7*24*60*60*1000;
+app.use( express.static( __dirname + '/public', { maxAge: cacheTime}) ) ;
 
 
 route.bind( app );
