f3070251bbb326f2263bb8320c3235ed18581521
teamrun <chenllos@163.com>
2014-06-12 09:38:57 +0800

    测试提纲

--- test/test_photo_upload.js
+++ test/test_photo_upload.js
@@ -12,5 +12,12 @@ var request = require('request')
  * 
  */
 
-// var reqParam
 
+/*
+ * read json file 配置好需要上传的数据
+ * 检查返回值
+ * get timeline 查看返回数据
+ * get 某个图片并保存查看
+ * 这样看的话这个测试文件好大啊```
+ * 
+ */
